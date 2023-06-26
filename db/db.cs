7fa0bf2652163d909e4784cc78859bc00d0dde77
MartijnGraafsma <91067234+MartijnGraafsma@users.noreply.github.com>
2023-06-26 14:15:25 +0200

    u

---
+++ db/db.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Extensions.Configuration;
+using MySql.Data.MySqlClient;
+using Dapper;
+using System.Collections.Generic;
+using System.Data;
+
+namespace Moodify.db
+{
+    public class db
+    {
+        private readonly IConfiguration configuration;
+
+        public db(IConfiguration config)
+        {
+            configuration = config;
+        }
+
+        public MySqlConnection GetConnection()
+        {
+            string connectionString = configuration.GetConnectionString("Default");
+            return new MySqlConnection(connectionString);
+        }
+    }
+}
