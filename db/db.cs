6591874b153c249fe7ea3b0cff304f96e3c60305
MartijnGraafsma <91067234+MartijnGraafsma@users.noreply.github.com>
2023-06-27 12:23:25 +0200

    poesj

--- db/db.cs
+++ db/db.cs
@@ -3,6 +3,9 @@ using MySql.Data.MySqlClient;
 using Dapper;
 using System.Collections.Generic;
 using System.Data;
+using Moodify.Models;
+using System.Data.SqlClient;
+using MongoDB.Driver.Core.Configuration;
 
 namespace Moodify.db
 {
@@ -17,8 +20,32 @@ namespace Moodify.db
 
         public MySqlConnection GetConnection()
         {
-            string connectionString = configuration.GetConnectionString("Default");
+            // todo: hier later de connection string ophalen uit de app.settings.
+            //string connectionString = configuration.GetConnectionString("Default");
+
+            // onderstaand de connectiostring nog invullen.
+            string connectionString = "Server=localhost; User ID=root; Database=moodify";
+
             return new MySqlConnection(connectionString);
+        }
+
+        public bool FindUserByUsernameAndPassword(UserModel user)
+        {
+
+            using (MySqlConnection connection = this.GetConnection())
+            {
+                string sql = "SELECT * FROM users WHERE username = @username AND password = @password";
+
+                using (MySqlCommand cmd = new(sql, connection))
+                {
+                    connection.Open();
+
+                    using (MySqlDataReader reader = cmd.ExecuteReader())
+                    {
+
+                    }
+                }
+            }
         }
     }
 }
